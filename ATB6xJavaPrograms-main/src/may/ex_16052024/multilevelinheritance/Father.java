f31c0745375e291626dccc054c7fe048b766e58f
Prem Kumar <premsit92@gmail.com>
2024-06-25 13:54:31 +0530

    Java Program

---
+++ ATB6xJavaPrograms-main/src/may/ex_16052024/multilevelinheritance/Father.java
@@ -0,0 +1,12 @@
+package src.may.ex_16052024.multilevelinheritance;
+
+public class Father extends GrandFather{
+    void home(){
+        System.out.println("2BHK");
+
+    }
+
+    void extra(){
+        System.out.println("Extra");
+    }
+}
